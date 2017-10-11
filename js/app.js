ed724ec233c84a236470b7a8ff64b47350665aec
ialami <alami.ismail_1995@yahoo.fr>
2017-10-11 16:45:11 +0100

    all styling centered

--- js/app.js
+++ js/app.js
@@ -1,55 +1,3 @@
-/*
-
-LOGIC OF THE GAME
-
-Think about the main events
-
-In terms of physics, what is the position of the ball at any time?
-
-Let p be the position of the ball,
-
-p(t=2) = p(t=1) + velocity
-p(t=2) = p(t=1) + speed * direction / direction either 1 or -1
-
-Therefore, in x and y-coordinates:
-p(t=2)[x] = p(t=1)[x] + speed * direction[x];
-p(t=2)[y] = p(t=1)[y] + speed * direction[y];
-
-In terms of coding that is everytime the ball touches either a paddle, either one of the top or bottom borders, the position of the ball needs to be updated through incrementation.
-
-
-How do you know when there is a collision between the ball and the paddle?
-
-Let L be the length of the paddles A and B.
-Paddles A and B can only move along the y-axis. Their x-position is therefore a constant.
-In order to check the collision of the ball at any time with one of the paddles, have an if statement. 3 conditions must be met, it is therefore condition1 && condition2 && condition 3:
-- The x position of the ball must be equal to the x-coordinate of the paddles
-- The y position of the ball must be lower than the y-coordinate of the top of the paddle
-- The y position of the ball must be greater than the y-coordinate of the top of the paddle
-If those 3 conditions happen in the same time, then there is collision, in which case the ball must change direction on the x-axis.
-
-In other words, if the ball hits one of the borders, then the y-direction of the ball changes but the X-direction does not change;
-if the ball hits one of the borders, then the x-direction changes, but the y-direction does not change;
-
-There are going to be 4 main events, when you press the arrow up or down (say for paddle A) and if you press two other key Q and A (say for paddle B). We also know that each key on the keyboard has a code, this is therefore not a problem. So basically that can be broken down into 2 events either up or down. If you press the up arrow and Q it respectively goes up for paddles A and B. If you press the down arrow and A, it goes respectively goes down for paddles A and B. All of these can be grouped into one function, which is basically the movement of the paddles.
-
-Events are:
-- $(document).on('keydown', function ())
-- $(document).on('keyup', function ())
-
-The next function can be the collision, which will trigger the movement of the ball
-
-And all of that is done within a set interval of lambda ms. So each lambda ms, it moves the ball according to the collision. Lamba must be small enough so that it does not miss collisions. Obviously there is always going to be some collisions that are going to be missed: because lambda would have to be really small.
-
-This is the MVP
-
-Potential for scale-up:
-- display the score and play games first to 11 or first to 21
-- Add sounds of chinese players shouting everytime it hits paddle A or B (that can be really funny to be honest)
-- Give an acceleration to the ball by incrementing it at every collision with a paddle.
-
-*/
-
 //declare global variables
 const directionVector = {
   x: 1,
@@ -107,6 +55,29 @@ function init () {
   $playertwo = $('.playertwo');
 
   $(window).on('load', playGASport);
+
+  /*
+$(window).on('load', runLogo);
+
+function runLogo () {
+  intervalLogo = setInterval(animateLogo, 1000)
+}
+
+function animateLogo () {
+  $shader.css('display', 'block');
+  $('#logo img').animate({
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '700px',
+    height: '700px'
+  }, 4000);
+}
+
+clearInterval(intervalLogo);
+  */
+
   $(document).on('keydown', movePaddles);
 
   // function startOfGame () {
@@ -123,15 +94,13 @@ function init () {
   //   });
   // }
   $button.on('click', playGame);
-
-  //setInterval to check for collision and keep bouncing the ball off
-  function playGame() {
-    commentaries = new Audio('/Users/ismailalami/Development/WDI_PROJECT_1/sounds/commentaries.mp3');
-    commentaries.play();
-    checkCollide = setInterval(checkCollision, 3);
-  }
 }
-// checkCollide = setInterval(checkCollision, 3);
+
+function playGame() {
+  commentaries = new Audio('/Users/ismailalami/Development/WDI_PROJECT_1/sounds/commentaries.mp3');
+  commentaries.play();
+  checkCollide = setInterval(checkCollision, 3);
+}
 
 function playGASport () {
   new Audio('/Users/ismailalami/Development/WDI_PROJECT_1/sounds/easportsound.mp3').play();
@@ -275,35 +244,6 @@ function resetScore () {
   scorep2=0;
 }
 
-//Inside Ball Movement function()
-//   check collision using if statements, or better using a switch statement.
-//
-//     give the ball a random direction at start
-//
-//     check collision with top border
-//       Is the y-position of the ball the same as the y-position of the top border?
-//     check collision with bottom border
-//       Is the y-position of the ball the same as the y-position of the bottom border?
-//     check collision with paddleA
-//       Is the x position of the ball equal to the x-coordinate of paddleA
-//       extract the top and bottom y coordinates of the paddleA
-//       Is the y position of the ball lower than the y-coordinate of the top of paddleA?
-//       Is the y position of the ball greater than the y-coordinate of the top paddleA?
-//     check collision with paddleB
-//       Is the x position of the ball equal to the x-coordinate of paddleB
-//       extract the top and bottom y coordinates of the paddleB
-//       Is the y position of the ball lower than the y-coordinate of the top of paddleB?
-//       Is the y position of the ball greater than the y-coordinate of the top paddleB?
-//     check collision with right border.
-//       Is the x-position of the ball the same as the x-position of the right border?
-//         If yes, put the ball back in the middle (i.e. start position) and restart the game.
-//     check collision with left border
-//       Is the x-position of the ball the same as the x-position of the left border?
-//         If yes, put the ball back in the middle (i.e. start position) and restart the game.
-//
-// note that there is an action that is repeated, which is to put the ball back in the middle, this can therefore be a function updateBallPosition () {}.
-//
-
 function displayAlert (player) {
   $alert = $('#alert');
   $ok = $('#ok');
@@ -325,3 +265,15 @@ function hideAlert () {
 function hitSound () {
   new Audio('/Users/ismailalami/Development/WDI_PROJECT_1/sounds/hit.mp3').play();
 }
+
+// function animateLogo () {
+//   // $shader.css('display', 'block');
+//   $('#logo img').animate({
+//     position: 'absolute',
+//     left: '50%',
+//     top: '50%',
+//     transform: 'translate(-50%, -50%)',
+//     width: '700px',
+//     height: '700px'
+//   }, 4000);
+// }
